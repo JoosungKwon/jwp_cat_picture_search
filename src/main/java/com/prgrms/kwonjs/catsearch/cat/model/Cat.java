e5eeb0d9330ea4cd86d3ccccad94d93163102e3e
kwonjoosung <kwonjoosung@gmail.com>
2023-04-05 04:31:13 +0900

    feat(Cat): 고양이 데이터를 저장하기 위한 엔티티 추가

---
+++ src/main/java/com/prgrms/kwonjs/catsearch/cat/model/Cat.java
@@ -0,0 +1,42 @@
+package com.prgrms.kwonjs.catsearch.cat.model;
+
+import static lombok.AccessLevel.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Cat {
+
+	@Id
+	private String id;
+
+	private String name;
+
+	private String url;
+
+	private Integer width;
+
+	private Integer height;
+
+	private String temperament;
+
+	private String origin;
+
+	@Builder
+	public Cat(String id, String name, String url, Integer width, Integer height, String temperament, String origin) {
+		this.id = id;
+		this.name = name;
+		this.url = url;
+		this.width = width;
+		this.height = height;
+		this.temperament = temperament;
+		this.origin = origin;
+	}
+}
