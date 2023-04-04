ae6427122b96e7bcce8ebf249e80be148c37b6ba
kwonjoosung <kwonjoosung@gmail.com>
2023-04-05 02:51:06 +0900

    init project

---
+++ src/main/java/com/prgrms/kwonjs/catsearch/CatSearchApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.kwonjs.catsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prgrms/kwonjs/catsearch/CatSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.kwonjs.catsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
