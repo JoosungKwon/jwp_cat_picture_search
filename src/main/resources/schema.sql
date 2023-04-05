b3df53aacf0e4f130e15ea92fcb26c4a85e428e1
kwonjoosung <kwonjoosung@gmail.com>
2023-04-05 23:57:09 +0900

    config(DB): 데이터 소스, JPA, Schema 설정
    - 스키마(DDL문) 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,16 @@
+-- init SQL
+-- create database app;
+-- use app;
+
+CREATE TABLE IF NOT EXISTS cat
+(
+    id          bigint       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    cat_id      varchar(255) NOT NULL UNIQUE,
+    url         varchar(500) NOT NULL,
+    width       int          NOT NULL,
+    height      int          NOT NULL,
+    breed_id    varchar(255) NULL,
+    breed_name  varchar(255) NULL,
+    temperament varchar(255) NULL,
+    origin      varchar(255) NULL
+);
