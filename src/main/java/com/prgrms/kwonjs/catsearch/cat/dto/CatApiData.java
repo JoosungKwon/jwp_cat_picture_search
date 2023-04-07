a5ee56883c15fcb09eb3eb6a7c84065e99212c85
kwonjoosung <kwonjoosung@gmail.com>
2023-04-07 18:20:06 +0900

    refactor(Cat): CatClient 수정
    - 요청에 대한 응답을 파싱하기 위해 CatApiData 추가
    - CatClient -> CatApiClient 클래스명 변경

---
+++ src/main/java/com/prgrms/kwonjs/catsearch/cat/dto/CatApiData.java
@@ -0,0 +1,47 @@
+package com.prgrms.kwonjs.catsearch.cat.dto;
+
+import java.util.List;
+
+import com.prgrms.kwonjs.catsearch.cat.model.Cat;
+import com.prgrms.kwonjs.catsearch.cat.model.vo.Breed;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CatApiData {
+
+	private String id;
+	private String url;
+	private Integer width;
+	private Integer height;
+	private List<Breed> breeds;
+
+	public CatApiData(String id, String url, Integer width, Integer height, List<Breed> breeds) {
+		this.id = id;
+		this.url = url;
+		this.width = width;
+		this.height = height;
+		this.breeds = breeds;
+	}
+
+	public static Cat toEntity(CatApiData catApiData) {
+		Breed breed = null;
+
+		if (catApiData.getBreeds() != null && !catApiData.getBreeds().isEmpty()) {
+			breed = catApiData.getBreeds().get(0);
+		}
+
+		return Cat.builder()
+			.catId(catApiData.getId())
+			.url(catApiData.getUrl())
+			.width(catApiData.getWidth())
+			.height(catApiData.getHeight())
+			.breed(breed)
+			.build();
+	}
+}
