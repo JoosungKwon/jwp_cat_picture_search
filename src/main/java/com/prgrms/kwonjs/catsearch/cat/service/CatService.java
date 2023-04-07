6c42608fd38ba2c7e142976ca6daf1f28a651f66
kwonjoosung <kwonjoosung@gmail.com>
2023-04-07 18:25:13 +0900

    refactor(Cat): CatService 수정
    - fillData를 통해 데이터를 가져올 수 있도록 하였음
    - 나머지 요청에 대해선 이미 가지고 있는 데이터(DB에 저장된)를 응답

--- src/main/java/com/prgrms/kwonjs/catsearch/cat/service/CatService.java
+++ src/main/java/com/prgrms/kwonjs/catsearch/cat/service/CatService.java
@@ -3,47 +3,58 @@ package com.prgrms.kwonjs.catsearch.cat.service;
 import java.net.URI;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.prgrms.kwonjs.catsearch.cat.dto.CatApiData;
 import com.prgrms.kwonjs.catsearch.cat.dto.CatResponse;
+import com.prgrms.kwonjs.catsearch.cat.dto.CatSimpleResponse;
 import com.prgrms.kwonjs.catsearch.cat.model.Cat;
 import com.prgrms.kwonjs.catsearch.cat.repository.CatRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class CatService {
 
-	public static final String SLASH = "/";
-	private final CatClient catClient;
+	public static final String CAT_NOT_FOUND = "해당하는 고양이가 없습니다.";
+
+	private final CatApiClient catClient;
 	private final CatRepository catRepository;
 
-	private URI DEFAULT_URI = URI.create("https://api.thecatapi.com/v1/images/search");
-	private @Value("api-key") String API_KEY;
+	private String REQUEST_URI = "https://api.thecatapi.com/v1/images/search/";
+	private String API_KEY = "live_mNhy71rfbMXZsW5pYW0c5eWHAZKeLqGIKyKaILKvmjno0H0bHVyaclVQfCKR6LHh";
 
-	public List<CatResponse> getRandom(int amount) {
-
-
-		catClient.get(DEFAULT_URI, API_KEY, amount);
-		return null;
+	public List<CatSimpleResponse> getRandom(int size) {
+		return catRepository.findByRandom(size)
+			.stream()
+			.map(CatSimpleResponse::of)
+			.toList();
 	}
 
-	public List<CatResponse> search(String breedId) {
-
-
-		catClient.search(DEFAULT_URI, API_KEY, breedId);
-		return null;
+	public List<CatSimpleResponse> search(String breedId) {
+		return catRepository.findByBreedId(breedId)
+			.stream()
+			.map(CatSimpleResponse::of)
+			.toList();
 	}
 
-	public Cat getBy(String catId) {
+	public CatResponse getBy(String catId) {
+		Cat cat = catRepository.findById(catId)
+			.orElseThrow(() -> new IllegalArgumentException(CAT_NOT_FOUND));
+		return CatResponse.of(cat);
+	}
 
-
-		URI requestUri = URI.create(DEFAULT_URI.toString() + SLASH + catId);
-		catClient.getById(requestUri, API_KEY);
-		return null;
+	@Transactional
+	public void fillData() {
+		log.info("start fill data");
+		List<CatApiData> responses = catClient.get(URI.create(REQUEST_URI), API_KEY, 100);
+		List<Cat> cats = responses.stream().map(CatApiData::toEntity).toList();
+		catRepository.saveAll(cats);
+		log.info("finish fill data");
 	}
 }
