b260cd389561adf49ddb9dd93fe151a8c44081ca
kwonjoosung <kwonjoosung@gmail.com>
2023-04-07 18:23:45 +0900

    feat(Cat): CatRepository custom 쿼리 추가
    - 랜덤 검색 쿼리
    - 품종을 통해 검색하는 쿼리

--- src/main/java/com/prgrms/kwonjs/catsearch/cat/repository/CatRepository.java
+++ src/main/java/com/prgrms/kwonjs/catsearch/cat/repository/CatRepository.java
@@ -1,8 +1,18 @@
 package com.prgrms.kwonjs.catsearch.cat.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.prgrms.kwonjs.catsearch.cat.model.Cat;
 
 public interface CatRepository extends JpaRepository<Cat, String> {
+
+	@Query(nativeQuery = true, value = "SELECT * FROM cat ORDER BY RAND() LIMIT :size")
+	List<Cat> findByRandom(@Param(value = "size") int size);
+
+	@Query(value = "SELECT c FROM Cat c WHERE c.breed.id = :breedId")
+	List<Cat> findByBreedId(@Param(value = "breedId") String breedId);
 }
