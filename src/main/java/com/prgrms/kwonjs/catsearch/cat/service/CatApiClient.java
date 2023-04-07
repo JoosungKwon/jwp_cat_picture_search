1479cc61ebab73748dcd1f3f81c3a45b41d7d2b8
kwonjoosung <kwonjoosung@gmail.com>
2023-04-08 06:12:25 +0900

    refactor(Feign): Feign 설정 변경 및 CatApiClient 메서드 변경
    - 불필요한 메서드 제거
    - api key 환경 변수 분리 및 Value 적용
    - requestInterceptor를 통해 api-key 공통 적용
    - Feign 로깅 설정

--- src/main/java/com/prgrms/kwonjs/catsearch/cat/service/CatApiClient.java
+++ src/main/java/com/prgrms/kwonjs/catsearch/cat/service/CatApiClient.java
@@ -1,35 +1,17 @@
 package com.prgrms.kwonjs.catsearch.cat.service;
 
-import java.net.URI;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.prgrms.kwonjs.catsearch.cat.dto.CatApiData;
 
-@FeignClient(name = "CatClient", url = "URL")
+@FeignClient(name = "CatClient", url = "${cat-api.default-url}")
 public interface CatApiClient {
 
-	@GetMapping
-	List<CatApiData> get(
-		URI requestUri,
-		@RequestHeader(name = "x-api-key") String apiKey,
-		@RequestParam(name = "limit") Integer limit
-	);
-
-	@GetMapping
-	List<CatApiData> search(
-		URI requestUri,
-		@RequestHeader(name = "x-api-key") String apiKey,
-		@RequestParam(name = "breedId") String breedId
-	);
-
-	@GetMapping
-	CatApiData getById(
-		URI requestUri,
-		@RequestHeader("x-api-key") String apiKey
-	);
+	@GetMapping(value = "/v1/images/search")
+	List<CatApiData> get(@RequestParam(value = "limit") int limit,
+		@RequestParam("has_breeds") int hasBreeds);
 }
--- src/main/java/com/prgrms/kwonjs/catsearch/common/config/FeignConfig.java
+++ src/main/java/com/prgrms/kwonjs/catsearch/common/config/FeignConfig.java
@@ -1,9 +1,27 @@
 package com.prgrms.kwonjs.catsearch.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import feign.Logger;
+import feign.RequestInterceptor;
 
 @Configuration
 @EnableFeignClients("com.prgrms.kwonjs.catsearch")
 class FeignConfig {
+
+	@Value("${cat-api.key}")
+	private String API_KEY;
+
+	@Bean
+	public RequestInterceptor requestInterceptor() {
+		return requestTemplate -> requestTemplate.header("x-api-key", API_KEY);
+	}
+
+	@Bean
+	Logger.Level feignLoggerLevel() {
+		return Logger.Level.FULL;
+	}
 }
