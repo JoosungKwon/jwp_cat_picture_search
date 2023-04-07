ca89081334f7ef0d161567d8d7b7a1c1f85b34b8
kwonjoosung <kwonjoosung@gmail.com>
2023-04-08 05:59:35 +0900

    feat: 데이터 로드를 위한 DataLoadManager 추가
    - CommandLineRunner 이용
    - 외부에서 값 주입시 해당하는 숫자만큼 데이터를 채울 수 있도록 함

---
+++ src/main/java/com/prgrms/kwonjs/catsearch/DataLoadManager.java
@@ -0,0 +1,73 @@
+package com.prgrms.kwonjs.catsearch;
+
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.prgrms.kwonjs.catsearch.cat.dto.CatApiData;
+import com.prgrms.kwonjs.catsearch.cat.repository.CatRepository;
+import com.prgrms.kwonjs.catsearch.cat.service.CatApiClient;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DataLoadManager implements CommandLineRunner {
+
+	private static final int DEFAULT_DATA_SIZE = 250;
+	private static final int REQUEST_DATA_SIZE = 100;
+	private static final int TRUE = 1;
+
+	private final CatApiClient catClient;
+	private final CatRepository catRepository;
+
+	/*
+	Data Loading
+	 */
+	@Override
+	@Transactional
+	public void run(String... args) { // 최초 실행 시점에 데이터를 채우는 메서드 실행
+
+		int dataSize = DEFAULT_DATA_SIZE;
+
+		if (args.length > 0) { // 인자를 통해서 데이터의 개수를 세팅할 수 있게 함
+			try {
+				dataSize = Integer.parseInt(args[0]);
+			} catch (NumberFormatException e) {
+				log.warn("Invalid Argument : {}", args[0]);
+				log.info("Use Default Data Size : {}", DEFAULT_DATA_SIZE);
+			}
+		}
+
+		long existDataSize = catRepository.count();
+
+		dataSize -= existDataSize;
+
+		if (dataSize <= 0) { // 채워야할 데이터의 개수가 0보다 작거나 같으면 이미 데이터가 채워져있는 것이므로 채우지 않음
+			log.info("already exist data : {} ", existDataSize);
+			return;
+		}
+
+		log.info("start fill data : need {}", dataSize);
+
+		while (dataSize > 0){ // 데이터를 100개씩 요청하여 채움
+			List<CatApiData> responses = catClient.get(REQUEST_DATA_SIZE, TRUE);
+
+			responses.stream()
+				.map(CatApiData::toEntity)
+				.forEach(catRepository::saveInsertIgnore);
+
+			dataSize -= responses.size();
+			log.info("filling data... : remain {} ", dataSize);
+		}
+
+		existDataSize = catRepository.count();
+
+		log.info("finish fill data : now {}", existDataSize);
+	}
+
+}
